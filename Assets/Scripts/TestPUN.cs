656c55485519b9f828492d50eb36d72b24ab8ae4
Shifat Khan <shifatkhan6@gmail.com>
2021-04-06 14:21:36 -0400

    Downgrade to Unity 2019
    This fixes Photon compatibility

---
+++ Assets/Scripts/TestPUN.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestPUN : MonoBehaviour
+{
+    // void Start()
+    // {
+    //     Connect();
+    //     CreateRoom();
+    //     JoinRandomRoom();
+    // }
+    //
+    // public void Connect()
+    // {
+    //     PhotonNetwork.ConnectUsingSettings(PlayerSettings.bundleVersion);
+    // }
+    //
+    // public void CreateRoom()
+    // {
+    //     PhotonNetwork.CreateRoom("Comp476 test room");
+    // }
+    //
+    // public void JoinRandomRoom()
+    // {
+    //     PhotonNetwork.JoinRandomRoom();
+    // }
+}
