e94b203b18d84cedf30aa21a7cc48c79c7de5d79
Shifat Khan <shifatkhan6@gmail.com>
2021-04-06 15:58:39 -0400

    Upgrade Photon to Photon2

--- Assets/Scripts/TestPUN.cs
+++ Assets/Scripts/TestPUN.cs
@@ -1,29 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Photon.Pun;
 
-public class TestPUN : MonoBehaviour
+public class TestPUN : MonoBehaviourPunCallbacks
 {
-    // void Start()
-    // {
-    //     Connect();
-    //     CreateRoom();
-    //     JoinRandomRoom();
-    // }
-    //
-    // public void Connect()
-    // {
-    //     PhotonNetwork.ConnectUsingSettings(PlayerSettings.bundleVersion);
-    // }
-    //
-    // public void CreateRoom()
-    // {
-    //     PhotonNetwork.CreateRoom("Comp476 test room");
-    // }
-    //
-    // public void JoinRandomRoom()
-    // {
-    //     PhotonNetwork.JoinRandomRoom();
-    // }
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        print("=== Connecting to name server... ===");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public void JoinRandomRoom()
+    {
+        print("=== Attempting to join a room... ===");
+        PhotonNetwork.JoinRandomRoom();
+    }
+    
+    public void CreateRoom()
+    {
+        print("=== Attempting to create a room... ===");
+        PhotonNetwork.CreateRoom("Comp476 test room");
+    }
+    
+    public override void OnConnected()
+    {
+        print("=== CONNECTED TO NAME SERVER ===");
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        print("=== CONNECTED TO MASTER ===");
+        JoinRandomRoom();
+    }
+    
+    public override void OnCreatedRoom()
+    {
+        print("=== ROOM CREATED ===");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("=== JOINED ROOM ===");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        print($"return code {returnCode}, message: {message}");
+        print($"=== Could NOT join a random room. Attempting to create a room instead... ===");
+        CreateRoom();
+    }
 }
